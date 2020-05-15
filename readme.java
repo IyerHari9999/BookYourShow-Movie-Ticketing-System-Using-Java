f71b40b7d932fd4b140f9e517bdd068e8684a6af
Hariharan R Iyer <55096592+IyerHari9999@users.noreply.github.com>
2020-05-15 17:47:51 +0530

    Update readme.java

--- readme.java
+++ readme.java
@@ -1,5 +1,5 @@
 class ReadMe {
     public static void main(String[] args) {
-        System.out.println(""); 
+        System.out.println("This project was made using NetBeans IDE 8.2"); 
     }
 }
